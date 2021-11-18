d837daa715429cafe4b8ec14a65b3907ebfa8529
Teja Medida <tejam@admins-MacBook-Pro.local>
2021-11-18 19:07:10 +0530

    added initial assignment for functions

---
+++ functions.js
@@ -0,0 +1,40 @@
+//Write a function to print Hello world
+function printHelloWorld() {
+    console.log("Hello world");
+}
+
+//Write a function to print given parameter
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+//Testing
+console.log("2 + 3 should be 5 and it is ", add(2,3));
+console.log("-4 + 4321 should be 4317 and it is ", add(-4,4321));
+console.log("-4 + -5 should be -9 and it is ", add(-4,-5));
+
+/*
+    Follow the above examples. Write functions for each given requirement.
+    MUST have appropriate names for the functions, parameters and variables
+    Write atleast 2 test scenarios
+*/
+
+//Write a function to add 2 numbers
+//Write a function to Multiply 2 numbers
+//Write a function to divide 2 numbers
+//Write a function to get reminder
+//Write a function to find the given number is positive
+//Write a function to find the given number is negative
+//Write a function to get the next immediate number using assignment operator
+//Write a function to get the next immediate number without using assignment operator
+//Write a function to get the before immediate number using assignment operator
+//Write a function to get the before immediate number without using assignment operator
+//Write a function to find the given 2 numbers are equal
+//Write a function to find the given argument type
+//Write a function to find greatest of 2 numbers
+//Write a function to find greatest of 3 numbers
+
+//Write a function to greet a given person
+//Example Greeting for Sweety: Hi Hello Namaste, Welcome Sweety
+//Write a function to print full name of a person, with given first and last names
+//Example full name for first name: Nagaraju, last name: Medida - Full name: Medida Nagaraju
