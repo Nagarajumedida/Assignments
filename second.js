f1d9c22e9b9caf9ce71756351e9282fdbb8b3764
Teja Meidda <tejameidda@Tejas-MacBook-Pro.local>
2021-12-07 18:27:30 +0530

    change

--- second.js
+++ second.js
@@ -33,3 +33,16 @@ function prime(num1) {
   }
 }
 console.log(prime(2));
+
+
+
+function primes(num1) {
+  for (var i = 2; i < num1; i++) {
+    if (num1 % i === 0) {
+      return false
+    }
+  }
+  return true
+}
+console.log(primes(17));
+console.log(primes(24));
