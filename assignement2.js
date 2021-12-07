98eb11a88e048511847422a18bd48ca0ad3bc912
Teja Meidda <tejameidda@Tejas-MacBook-Pro.local>
2021-12-07 11:46:56 +0530

    second attempted

--- assignement2.js
+++
@@ -1,3 +0,0 @@
-// Write a function to tell wether given number is even or not
-// Write a function to tell wether given number is odd or not
-// Write a function to tell wether given number is prime number or not
---
+++ second.js
@@ -0,0 +1,39 @@
+// Write a function to tell wether given number is even or note
+function even(num1) {
+  if (num1%2 === 0) {
+    console.log(true);
+    return " num1 is a even number"
+  }
+  else {
+    console.log(false);
+    return " num1 is not a even number"
+  }
+}
+console.log(even(4));
+console.log(even(5));
+// Write a function to tell wether given number is odd or not
+function odd(num1) {
+  if (num1%2 != 0) {
+    console.log(true);
+    return " num1 is a odd number"
+  }
+  else {
+    console.log(false);
+    return " num1 is not a odd number"
+  }
+}
+console.log(odd(5));
+console.log(odd(6));
+
+// Write a function to tell wether given number is prime number or not
+function prime(num1) {
+  if (num1 === 2) {
+    console.log(true);
+    return " num1 is a prime number"
+  }
+  else {
+    console.log(false);
+    return " num1 is not a prime number"
+  }
+}
+console.log(prime(2));
